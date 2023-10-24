78b67d78ab048dd5a63e9e1716d879884e5071dc
2020037335@student.unza.zm <2020048078@student.unza.zm>
2023-10-24 16:22:49 +0200

    Noel Simpemba made this third commit

--- input_scraper.py
+++ input_scraper.py
@@ -6,5 +6,16 @@ import requests
 import pandas as pd 
 from bs4 import BeautifulSoup
 
-
 # Isaac Shapi ended here 
+
+
+# Noel Simpemba started here 
+
+# Define the URL of the digital library website 
+# Send an HTTP request and parse the HTML
+url = 'https://example.com/digital-library'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Noel Simpemba ended here 
+
