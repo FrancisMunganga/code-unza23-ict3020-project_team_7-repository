07bab5967d24c57ee074b661a6ed3c629a8ea081
Simpemba Noel <2020037335@student.unza.zm>
2023-10-24 16:48:41 +0200

    Noel Simpemba made this commit

--- input_scraper.py
+++ input_scraper.py
@@ -17,5 +17,26 @@ url = 'https://example.com/digital-library'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# Noel Simpemba ended here 
+# Noel Simpemba ended here
 
+
+# Noel Simpemba started here 
+
+# Find all the <tbody> elements that contain the metadata elements
+
+tbodies = soup.find_all("tbody")
+
+# Create an empty list to store the scraped data
+
+data = []
+
+# Loop through each <tbody> element
+
+for tbody in tbodies:
+
+    # Create an empty dictionary to store the metadata of one item
+
+    item = {}
+
+
+# Noel Simpemba ended here     
