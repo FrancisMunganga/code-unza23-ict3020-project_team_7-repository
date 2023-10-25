9c8cf60d157beabac55a178177e6bfc7c69da631
Isaac Shapi <2020048078@student.unza.zm>
2023-10-25 15:09:24 +0200

    Isaac made this commit

---
+++ app.py
@@ -0,0 +1,8 @@
+# Isaac Shapi started coding here 
+# import the libraries 
+
+from flask import Flask, request
+from bs4 import BeautifulSoup
+import requests
+import csv
+# Isaac Shapi ended here 
