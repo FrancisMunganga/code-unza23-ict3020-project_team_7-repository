679d722e85efcd8387de04eeccfc6f6a35c50084
Noel-Simpemba <2020037335@student.unza.zm>
2023-10-25 15:32:42 +0200

    Noel made this commit

--- app.py
+++ app.py
@@ -16,3 +16,10 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def scrape():
 # Jane Yowela ended here 
+
+# Noel Simpemba started editing here 
+    if request.method == 'POST':
+        url = request.form['url']
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+# Noel Simpemba ended here 
