074a0a2541da1e66f7ba7fafa88f5b4af62b1a60
FrancisMunganga <2020010054@student.unza.zm>
2023-10-25 15:38:32 +0200

    Francis made this commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import csv
 # Isaac Shapi ended here 
 
 
-# # Jane Yowela started here 
+# # Jane Yowela started editing here 
 # Using flask to create the app
 
 app = Flask(__name__)
@@ -17,9 +17,20 @@ app = Flask(__name__)
 def scrape():
 # Jane Yowela ended here 
 
+
 # Noel Simpemba started editing here 
     if request.method == 'POST':
         url = request.form['url']
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
 # Noel Simpemba ended here 
+
+
+# Francis Munganga started editing here 
+        # Find metadata
+        meta = soup.find_all('meta')
+
+        # Define CSV file
+        filename = "metadata.csv"
+        csv_writer = csv.writer(open(filename, 'w'))
+#Francis Munganga ended here 
